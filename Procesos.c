a7933c47e14d20a3a179199a3f1d264965fdf98d
Beatriz <Beatriz.latorre@sansano.usm.cl>
2019-10-06 19:18:23 -0300

    Update Procesos.c
    Tuberias (pipes) colocadas

--- Procesos.c
+++ Procesos.c
@@ -9,18 +9,48 @@
 
   int procesosYTurnos(){
 	turno = 0;
-	pid = fork();
 	pipe(p01);
+	pipe(p10);
+	pipe(p03);
+	pipe(p30);
+	pid = fork();
 
 	if(pid == 0){
 		turno = 1;
+		close(p01[1]);
+		close(p10[0]); 
+		pipe(p12);
+		pipe(p21);
 		pid = fork();
 		if(pid == 0){
 			turno = 2;
+			close(p12[1]);
+			close(p21[0]);
+			pipe(p23);
+			pipe(p32);
 			pid = fork();
+			if(pid == 0){
+				close(p23[1]);
+				close(p32[0]);
+				close(p03[1]);
+				close(p30[0]);
+				turno = 3;	
+			}
+			else{
+				close(p23[0]);
+				close(p32[1]);
+			}
+		}
+		else{
+			close(p12[0]);
+			close(p21[1]);
 		}
 	}
 	else{
+		close(p01[0]);
+		close(p10[1]);
+		close(p03[0]);
+		close(p30[1]);
 		turno = 0;
 	}
 	return turno;
