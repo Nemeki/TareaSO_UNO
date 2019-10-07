ac862179c2eb26eaf4df9822ce583226cfd27076
Beatriz <Beatriz.latorre@sansano.usm.cl>
2019-10-06 22:38:51 -0300

    Update Procesos.c
    Procesos se crean, generan tuberias y administran turnos

--- Procesos.c
+++ Procesos.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
 
-  pid_t pid;
-  int idJugador;
-  int p01[2], p10[2], p12[2], p21[2], p23[2], p32[2], p30[2], p03[2];
-  
 
-  int procesoseIDJugador(){
+
+void procesos(){
+	pid_t pid;
+	int idJugador;
+	int p01[2], p10[2], p12[2], p21[2], p23[2], p32[2], p30[2], p03[2];
+ 
+	bool jugando, reversa;
+	int readbytes;
+	char buffer[SIZE];
+
+	jugando = TRUE;
+	reversa = FALSE;
+
 	idJugador = 0;
 	pipe(p01);
 	pipe(p10);
@@ -53,28 +61,148 @@
 		close(p30[1]);
 		idJugador = 0;
 	}
-	return idJugador;
-  }
-  
-  void escribirMensaje(char mensaje[50], int pipe[2]){
-	char buffer[SIZE];
-	strcpy(buffer, mensaje);
-	write( b[1], buffer, strlen(buffer));
-	close( b[1] );
-  }
-
-  void turnos(int id){
-	bool jugando, reversa;
-	jugando = TRUE;
-	reversa = FALSE;
-	if(id == 0){
+	
+	if(idJugador == 0){
 		//Jugar turno.
+		//pipe normal
+		if(reversa == TRUE){
+			strcpy(buffer, "turno acabado");
+			write( p03[1], buffer, strlen(buffer));
+			close( p03[1]);
+			}
+		else{
+			strcpy(buffer, mensaje);
+			write( p01[1], buffer, "turno acabado");
+			close( p01[1]);
+			}
+		}
+	while(jugando){
+		if(reversa == TRUE){
+			switch (idJugador){
+				case 0:
+					readbytes = read(p10[0], buffer, SIZE)
+					while( readbytes > 0){
+						write(1, buffer, readbytes);
+						}
+					close(p10[0]);
+					break;
+
+				case 1:
+					readbytes = read(p21[0], buffer, SIZE)
+					while( readbytes > 0){
+						write(1, buffer, readbytes);
+						}
+					close(p21[0]);
+					break;
+
+				case 2:
+					readbytes = read(p32[0], buffer, SIZE)
+					while( readbytes > 0){
+						write(1, buffer, readbytes);
+						}
+					close(p32[0]);
+					break;
+
+				case 3:
+					readbytes = read(p03[0], buffer, SIZE)
+					while( readbytes > 0){
+						write(1, buffer, readbytes);
+						}
+					close(p03[0]);
+					break;
+				}
+			}
+		else{
+			switch (idJugador){
+				case 0:
+					readbytes = read(p30[0], buffer, SIZE)
+					while( readbytes > 0){
+						write(1, buffer, readbytes);
+						}
+					close(p30[0]);
+					break;
+
+				case 1:
+					readbytes = read(p01[0], buffer, SIZE)
+					while( readbytes > 0){
+						write(1, buffer, readbytes);
+						}
+					close(p01[0]);
+					break;
+
+				case 2:
+					readbytes = read(p12[0], buffer, SIZE)
+					while( readbytes > 0){
+						write(1, buffer, readbytes);
+						}
+					close(p12[0]);
+					break;
+
+				case 3:
+					readbytes = read(p23[0], buffer, SIZE)
+					while( readbytes > 0){
+						write(1, buffer, readbytes);
+						}
+					close(p23[0]);
+					break;
+				}
+			}
+		
+		//Jugar
 
 		if(reversa == TRUE){
-		
+			switch (idJugador){
+				case 0:
+					strcpy(buffer, "turno acabado");
+					write( p03[1], buffer, strlen(buffer));
+					close( p03[1]);
+					break;
+
+				case 1:
+					strcpy(buffer, "turno acabado");
+					write( p10[1], buffer, strlen(buffer));
+					close( p10[1]);
+					break;
+
+				case 2:
+					strcpy(buffer, "turno acabado");
+					write( p21[1], buffer, strlen(buffer));
+					close( p21[1]);
+					break;
+
+				case 3:
+					strcpy(buffer, "turno acabado");
+					write( p32[1], buffer, strlen(buffer));
+					close( p32[1]);
+					break;
+				}
+			}
+		else{
+			switch (idJugador){
+				case 0:
+					strcpy(buffer, "turno acabado");
+					write( p01[1], buffer, strlen(buffer));
+					close( p01[1]);
+					break;
+
+				case 1:
+					strcpy(buffer, "turno acabado");
+					write( p12[1], buffer, strlen(buffer));
+					close( p12[1]);
+					break;
+
+				case 2:
+					strcpy(buffer, "turno acabado");
+					write( p23[1], buffer, strlen(buffer));
+					close( p23[1]);
+					break;
+
+				case 3:
+					strcpy(buffer, "turno acabado");
+					write( p30[1], buffer, strlen(buffer));
+					close( p30[1]);
+					break;
+				}
+			}
 		}
 	}
-	while jugando{
-	
-	}
-  }
